66c479550956c46a1ca850588dd53db7ec8942c2
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2024-12-16 12:55:45 +0100

    remove comments

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ const listItems = document.querySelectorAll("li");
 const links = document.querySelectorAll("a");
 const header = document.querySelector("header");
 
-// Initialize theme from localStorage
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark-theme");
   welcome.classList.add("dark-theme");
@@ -20,23 +19,17 @@ if (localStorage.getItem("theme") === "dark") {
   iconMoon.style.display = "none";
 }
 
-// Add toggle event
 themeToggle.addEventListener("click", () => {
   const isDarkMode = body.classList.toggle("dark-theme");
 
-  // Toggle icons
   iconSun.style.display = isDarkMode ? "inline-block" : "none";
   iconMoon.style.display = isDarkMode ? "none" : "inline-block";
 
-  // Toggle dark theme for cards and list items
   cards.forEach((card) => card.classList.toggle("dark-theme", isDarkMode));
   listItems.forEach((item) => item.classList.toggle("dark-theme", isDarkMode));
-
-  // Toggle dark theme for links, welcome and header
   links.forEach((link) => link.classList.toggle("dark-theme", isDarkMode));
   welcome.classList.toggle("dark-theme", isDarkMode);
   header.classList.toggle("dark-theme", isDarkMode);
 
-  // Save theme preference to localStorage
   localStorage.setItem("theme", isDarkMode ? "dark" : "light");
 });
